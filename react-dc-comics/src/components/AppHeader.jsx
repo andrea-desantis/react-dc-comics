2d1c6b84499df136176d740ef8879c2ba9d043f0
andrea-desantis <andrea.desantis290@gmail.com>
2025-11-25 18:40:55 +0100

    feat: App Header

---
+++ react-dc-comics/src/components/AppHeader.jsx
@@ -0,0 +1,25 @@
+function AppHeader() {
+    return(
+        <header>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </header>
+    );
+}
+
+export default AppHeader;
