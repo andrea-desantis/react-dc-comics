97e68ec336d04674cfbd88d4aee89c2b31dfef19
andrea-desantis <andrea.desantis290@gmail.com>
2025-12-02 00:28:24 +0100

    fix: cards

--- react-dc-comics/src/components/AppMain.jsx
+++ react-dc-comics/src/components/AppMain.jsx
@@ -1,26 +1,32 @@
 import './AppMain.css';
 import comics from '../comics.js';
 function AppMain() {
-    return(
-        <main >
+    return (
+        <main className='bg-main'>
+
             <div className='bg-comics'>
                 <div className='cur-ser'>
                     <h2>CURRENT SERIES</h2>
                 </div>
             </div>
-            <div className='bg-main'>
-                <ul className='flex wrap'>
-                    {comics.map((comic, i) => (
-                        <li key={i}>
-                            <img src={comic.thumb} alt="" />
-                            <span>
-                                {comic.series}
-                            </span>
-                        </li>
-                    ))}
-                </ul>
-            </div>
 
+            <div className='marginX'>
+                <div className=''>
+                    <ul className='flex wrap gap-20'>
+                        {comics.map((comic, i) => (
+                            <li key={i} className='cards'>
+                                <div className='img-box'>
+                                    <img src={comic.thumb} alt="" />
+                                </div>
+                                
+                                <span>
+                                    {comic.series}
+                                </span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </main>
     );
 }
