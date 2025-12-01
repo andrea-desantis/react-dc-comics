7025227a55f28e0a7a44dba60b5e757ae7350c2e
andrea-desantis <andrea.desantis290@gmail.com>
2025-12-02 00:06:33 +0100

    feat: comics card

--- react-dc-comics/src/components/AppMain.jsx
+++ react-dc-comics/src/components/AppMain.jsx
@@ -1,10 +1,25 @@
 import './AppMain.css';
-
+import comics from '../comics.js';
 function AppMain() {
     return(
-        <main className='bg-main'>
-            
-            <div>I CONTENUTI VANNO QUI</div>
+        <main >
+            <div className='bg-comics'>
+                <div className='cur-ser'>
+                    <h2>CURRENT SERIES</h2>
+                </div>
+            </div>
+            <div className='bg-main'>
+                <ul className='flex wrap'>
+                    {comics.map((comic, i) => (
+                        <li key={i}>
+                            <img src={comic.thumb} alt="" />
+                            <span>
+                                {comic.series}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
 
         </main>
     );
